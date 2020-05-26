c644db08d4059dcec222ea7d4b8ba8c5572a0099
Sandra <sandrajanineo@gmail.com>
2020-05-26 11:59:16 -0400

    integrated firebase/firestore

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 
+import { config } from './dbConfig';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
 import useCachedResources from './hooks/useCachedResources';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import LinkingConfiguration from './navigation/LinkingConfiguration';
@@ -15,6 +19,9 @@ export default function App(props) {
   if (!isLoadingComplete) {
     return null;
   } else {
+    firebase.initializeApp(config);
+    const db = firebase.firestore();
+
     return (
       <View style={styles.container}>
         {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
