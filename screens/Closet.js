cde6ec769d96174acb76e1109e501d15e32328ee
Sandra <sandrajanineo@gmail.com>
2020-08-07 18:13:57 -0400

    - added an Add Items button to the Closet screen

--- screens/Closet.js
+++ screens/Closet.js
@@ -1,19 +1,49 @@
 import * as React from 'react';
-import { ScrollView } from 'react-native-gesture-handler';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import Carousel from '../components/Carousel';
 import { GlobalContext } from '../hooks/global';
 
 export default function Closet ({ navigation }) {
-  const { fetchCollectionTypes, types, userID } = React.useContext( GlobalContext );
+  const { fetchCollectionTypes, types, userID, resetState } = React.useContext( GlobalContext );
 
   React.useEffect(() => {
     return fetchCollectionTypes( userID );
   }, [] );
 
   return (
-    <Carousel types={ types } navigation={ navigation } />
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          resetState( 'success' );
+          navigation.navigate( 'Add Item' );
+        }}
+      >
+        <Text style={styles.buttonText}>Add Items</Text>
+      </TouchableOpacity>
+      <Carousel types={ types } navigation={ navigation } />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#48D1CC',
+  },
+  button: {
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 2,
+    marginBottom: 10,
+    marginTop: 30,
+    margin: '10%',
+    padding: 10,
+  },
+  buttonText: {
+    fontSize: 22,
+    textAlign: 'center',
+  },
+})
 
